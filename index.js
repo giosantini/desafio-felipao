82c3be7d578e28543df78a3167e496b4b64ef525
giosantini <giovannisantini98@gmail.com>
2023-12-19 18:50:59 -0300

    quase finalizado

--- index.js
+++ index.js
@@ -1,11 +1,20 @@
 const mensagem = "seu herói de nome "
 const mensagemii = "seu herói de nome "
 
-let nomeHeroi = "giovanni"
-let XP
+let nomeHeroi 
+let XP 
 let nivel 
 
 if(XP <= 1000)
 {
-    nivel = 'bronze'
+    nivel = "Ferro"
 }
+else if(XP >= 1001 &&  XP <= 2000)
+{
+    nivel = "Bronze"
+}
+else if(XP >= 2001 && XP <= 5000)
+{
+    nivel = "prata"
+}
+
