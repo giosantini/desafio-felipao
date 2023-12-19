2370483de1af856f6a67bc8aa560c3484383f868
giosantini <giovannisantini98@gmail.com>
2023-12-19 16:51:48 -0300

    testando

--- index.js
+++ index.js
@@ -2,11 +2,10 @@ const mensagem = "seu herói de nome "
 const mensagemii = "seu herói de nome "
 
 let nomeHeroi = "giovanni"
-let XP = 1500
+let XP
 let nivel 
 
 if(XP <= 1000)
 {
-    console.log(mensagem + nomeHeroi, mensagemii + XP)
-
+    nivel = 'bronze'
 }
