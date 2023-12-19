20b35802c5289923b6e0d54cf14708995fb1d598
giosantini <giovannisantini98@gmail.com>
2023-12-19 15:30:21 -0300

    testando

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 let nome = "giovanni"
 let XP = 1000
 
-if(XP < 1000)
+if(XP <= 1000)
 {
-    console.log("seu herói de nome" + nome, "esta no nível de" + XP)
+    console.log("seu herói de nome " + nome, "esta no nível de " + XP)
 }
